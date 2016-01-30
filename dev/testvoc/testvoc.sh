d4457313d75a7f6b4ff9bada6d19b3e6ebc79cf2
Wolfgang Thauer <wolfgangwth@web.de>
2016-01-30 13:42:50 +0000

    added TMPDIR variable

--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -1,19 +1,20 @@
+TMPDIR=/tmp
+
 if [[ $1 = "eng-deu" ]]; then
 
   echo "==English->German===========================";
-  bash inconsistency.sh eng-deu > /tmp/eng-deu.testvoc;
-  bash inconsistency-summary.sh /tmp/eng-deu.testvoc eng-deu
+  bash inconsistency.sh eng-deu > $TMPDIR/eng-deu.testvoc;
+  bash inconsistency-summary.sh $TMPDIR/eng-deu.testvoc eng-deu
   echo ""
 
 elif [[ $1 = "deu-eng" ]]; then
 
   echo "==German->English===========================";
-  bash inconsistency.sh deu-eng > /tmp/deu-eng.testvoc;
-  bash inconsistency-summary.sh /tmp/deu-eng.testvoc deu-eng
+  bash inconsistency.sh deu-eng > $TMPDIR/deu-eng.testvoc;
+  bash inconsistency-summary.sh $TMPDIR/deu-eng.testvoc deu-eng
   echo ""
 
 else
   echo ""
   echo "==unknown language pair=====================";
 fi
-
