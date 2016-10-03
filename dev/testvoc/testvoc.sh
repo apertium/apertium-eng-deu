8a9975458e7c12fca9c74a4880f36585eee92079
nkrot <nkrot@oqaa.projectjj.com>
2016-10-03 16:09:43 +0000

    - code refactored to remove code duplication - code refactored to allow running the script from any location (before: only in .) - help message and option --help added

--- dev/testvoc/testvoc.sh
+++ dev/testvoc/testvoc.sh
@@ -1,20 +1,84 @@
+#!/bin/bash
+
+# # #
+# TODO
+# 1. use mktemp for generating tmp file names and clean up on exit
+# 2. inconsistency-summary.sh writes a file! get rid of it
+#
+
 TMPDIR=/tmp
+SCRIPTDIR="$(cd "$(dirname "$BASH_SOURCE")"; pwd)"
+OPT_DATADIR=
 
-if [[ $1 = "eng-deu" ]]; then
+show_help() {
+    local myname=`basename $0`
+    cat <<EOF
 
-  echo "==English->German===========================";
-  bash inconsistency.sh eng-deu > $TMPDIR/eng-deu.testvoc;
-  bash inconsistency-summary.sh $TMPDIR/eng-deu.testvoc eng-deu
-  echo ""
+  Check consistency between the monolingual dictionary on the source side
+and the biligual dictionary in the given language pair.
+USAGE: $myname [OPTIONS] translation-direction(s)
+for example:
+USAGE: $myname eng-deu
+USAGE: $myname -d path/to/apertium-eng-deu eng-deu deu-eng
+OPTIONS:
+  --help   display this message and exit
+  -d|--datadir[=]PATH
+           this option is passed down to inconsistency.sh script. Read more
+           in --help for the latter script.
 
-elif [[ $1 = "deu-eng" ]]; then
+EOF
+}
 
-  echo "==German->English===========================";
-  bash inconsistency.sh deu-eng > $TMPDIR/deu-eng.testvoc;
-  bash inconsistency-summary.sh $TMPDIR/deu-eng.testvoc deu-eng
-  echo ""
+process() {
+    local langpair=$1
+    local tmpfile=$TMPDIR/$langpair.testvoc
 
-else
-  echo ""
-  echo "==unknown language pair=====================";
+    bash $SCRIPTDIR/inconsistency.sh $OPT_DATADIR "$langpair" > "$tmpfile"
+    kode=$?
+    if [ $kode -ne 0 ]; then
+	echo "ERROR: An error (exit code ${kode}) occurred at the first stage of analysis." >&2
+	return 1
+    fi
+
+    bash $SCRIPTDIR/inconsistency-summary.sh "$tmpfile" "$langpair"
+}
+
+if [ $# -eq 0 ]; then
+    show_help
+    exit 1
 fi
+
+while [ $# -ne 0 ]; do
+    case $1 in
+	-h|--help)
+	    show_help
+	    exit 0
+	    ;;
+	-d|--datadir|--data-dir)
+	    OPT_DATADIR="$1 $2"
+	    shift
+	    ;;
+	-d*|--datadir=*|--data-dir=*)
+	    OPT_DATADIR="$1"
+	    ;;
+	-*)
+	    echo "ERROR: Unrecognized option '$1'" >&2
+	    exit 2
+	    ;;
+	eng-deu)
+	    echo "==English->German==========================="
+	    process $1
+	    echo ""
+	    ;;
+	deu-eng)
+	    echo "==German->English==========================="
+	    process $1
+	    echo ""
+	    ;;
+	*)
+	    echo "ERROR: Unknown language pair: '$1'" >&2
+	    exit 3
+	    ;;
+    esac
+    shift
+done
